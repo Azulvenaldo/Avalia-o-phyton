c44816a0aa266e04e38c5c6a2430ff6c1b9a392e
Azulvenaldo <marcushenriqueoliveiradearaujo@gmail.com>
2025-08-12 20:03:51 -0300

    Add files via upload

---
+++ avaliação.py
@@ -0,0 +1,27 @@
+
+saldo = 200.0
+
+senha = input("Digite a senha para acessar o caixa eletronico: ")
+
+while True:
+    print("1. Consultar saldo")
+    print("2. Depositar dinheiro")
+    print("3. Sacar dinheiro")
+    print("4. Sair")
+    opcao =input("Escolha uma opção: ")
+
+    if opcao == "1":
+        print(f"Saldo: R$ {saldo:.2f}")
+    elif opcao == "2":
+        valor = float(input("Valor para deposito: "))
+        saldo += valor
+    elif opcao == "3":
+        valor = float(input("Valor para saque: "))
+        if valor <= saldo:
+            saldo -= valor
+        else:
+            print("Saldo insuficiente. ")
+    elif opcao == "4":
+        break
+    else:
+        print("Opção invalida. ")
